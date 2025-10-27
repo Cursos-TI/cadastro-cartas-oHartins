04bc1894fe10276e0b5652ebda2dcdb347cdc349
oHartins <thiagohermesmr@gmail.com>
2025-10-27 16:22:42 +0000

    super_trunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,38 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado;
+  char codigo da carta[3];
+  char nome da cidade[50];
+  int populacao;
+  float area;
+  float pib per capita;
+  int numero de pontos turisticos;
+  
   // Área para entrada de dados
+  printf("Digite o estado: ");
+  scanf("%s", &estado);
+  printf("Digite o código da carta: ");
+  scanf("%s", &codigo da carta);
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &nome da cidade);
+  printf("Digite a população: ");
+  scanf("%d", &populacao);
+  printf("Digite a área (em km²): ");
+  scanf("%f", &area);
+  printf("Digite o PIB per capita: ");
+  scanf("%f", &pib per capita);
+  printf("Digite o número de pontos turísticos: ");
+  scanf("%d", &numero de pontos turisticos);
 
   // Área para exibição dos dados da cidade
-
+  printf("\n--- Carta da Cidade ---\n");
+  printf("Estado: %s\n", estado);
+  printf("Código da Carta: %s\n", codigo da carta);
+  printf("Nome da Cidade: %s\n", nome da cidade);
+  printf("População: %d\n", populacao);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB per capita: %.2f\n", pib per capita);
+  printf("Número de Pontos Turísticos: %d\n", numero de pontos turisticos);
 return 0;
 } 
